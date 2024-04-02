a5a1a0931a04a11c0a2abd6ea5d3a6cec5d345fa
concerned-doggo <codeformetoo@proton.me>
2024-04-02 21:48:14 +0530

    add ui files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./main.js",
+        "./A-F.html",
+    "./src/**/*.{js,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
