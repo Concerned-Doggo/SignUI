4abff012adaef796a3c8db6f219bd3cdcb4d6f2e
concerned-doggo <codeformetoo@proton.me>
2024-04-09 16:42:30 +0530

    add image change on LearnPage

--- LearnPages/A-F.js
+++ LearnPages/A-F.js
@@ -1,117 +1,135 @@
-// import * as tf from '@tensorflow/tfjs';
-// import * as tmImage from '@teachablemachine/image';
 // MODEL LINK
-    const URL = "http://localhost:5173/A_F-model/";
-    const videoElement = document.getElementsByClassName('input_video')[0] ;
-    const canvasElement = document.getElementsByClassName('output_canvas')[0];
-    const canvasCtx = canvasElement.getContext('2d');
-    let labelContainer = document.getElementById("label-container");
+const URL = "http://localhost:5173/A_F-model/";
+const aplhabetImage = document.getElementById('alphabetImage');
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
+let labelContainer = document.getElementById("label-container");
 
 
-    let model, webcam, maxPredictions;
+// Initializing 
+const initilize_btn = document.getElementById("initialize-btn");
+initilize_btn.addEventListener("click", init);
+
+
+let model, webcam, maxPredictions;
+const aplhabets = ["A", "B", "C", "D", "E", "F"];
+
 
 console.log("A-F Loaded")
-    // calling holistic api from mediapipe cdn
-    const holistic = new Holistic({locateFile: (file) => {
-      return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
-    }});
-    // console.log("Holistic loaded")
+// calling holistic api from mediapipe cdn
+const holistic = new Holistic({
+    locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
+    }
+});
+// console.log("Holistic loaded")
 
-      // Load the image model and setup the webcam
-    async function init() {
+// Load the image model and setup the webcam
+let aplhabetIndex = 0;
+aplhabetImage.src = "../Assets/Alphabets/" + aplhabets[aplhabetIndex] + ".png";
+
+async function init() {
     console.log("called init")
-        const modelURL = URL + "model.json";
-        const metadataURL = URL + "metadata.json";
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
 
-        // load the model and metadata
-        model = await tmImage.load(modelURL, metadataURL);
-        maxPredictions = model.getTotalClasses();
-        console.log("model loaded")
-        
-        // append elements to the DOM
-        // document.getElementById("webcam-container").appendChild(canvasElement);
-        // labelContainer = document.getElementById("label-container");
-        for (let i = 0; i < maxPredictions; i++) { // and class labels
-            labelContainer.appendChild(document.createElement("div"));
-        }
+    // load the model and metadata
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+    console.log("model loaded")
+
+    // append elements to the DOM
+    // document.getElementById("webcam-container").appendChild(canvasElement);
+    // labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
+        labelContainer.appendChild(document.createElement("div"));
+    }
 
     console.log(labelContainer.innerHTML);
-        // configuring our holistic api according to webcam and requirements
-        holistic.setOptions({
-          modelComplexity: 1,
-          smoothLandmarks: true,
-          enableSegmentation: true,
-          smoothSegmentation: true,
-          refineFaceLandmarks: true,
-          minDetectionConfidence: 0.5,
-          minTrackingConfidence: 0.5
-        });
-  
-        // creating an camera using holistic api 
-        const camera = new Camera(videoElement, {
-          onFrame: async () => {
-            await holistic.send({image: videoElement});
-          },
-          width: 1280,
-          height: 720
-        });
+    // configuring our holistic api according to webcam and requirements
+    holistic.setOptions({
+        modelComplexity: 1,
+        smoothLandmarks: true,
+        enableSegmentation: true,
+        smoothSegmentation: true,
+        refineFaceLandmarks: true,
+        minDetectionConfidence: 0.5,
+        minTrackingConfidence: 0.5
+    });
 
-        camera.start();
-        console.log("Camera element created")
+    // creating an camera using holistic api 
+    const camera = new Camera(videoElement, {
+        onFrame: async () => {
+            await holistic.send({ image: videoElement });
+        },
+        width: 1280,
+        height: 720
+    });
 
-        // on detecting webcam image draw landmarks
-        window.requestAnimationFrame(loop);
-        // holistic.onResults(draw); //change
-      }
+    camera.start();
+    console.log("Camera element created")
 
-    async function loop() {
-        // webcam.update(); // update the webcam frame
-        holistic.onResults(draw);
-        await predict();
-        window.requestAnimationFrame(loop);
+    // on detecting webcam image draw landmarks
+    window.requestAnimationFrame(loop);
+    // holistic.onResults(draw); //change
+}
+
+async function loop() {
+    // webcam.update(); // update the webcam frame
+    holistic.onResults(draw);
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+// run the webcam image through the image model
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(canvasElement);
+    let maxIndex = 0;
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+        labelContainer.childNodes[i].innerHTML = classPrediction;
+        if(prediction[i].probability > prediction[maxIndex].probability){
+            maxIndex = i;
+        }
     }
-
-    // run the webcam image through the image model
-    async function predict() {
-      // predict can take in an image, video or canvas html element
-      const prediction = await model.predict(canvasElement);
-    let maxi = 0;
-      for (let i = 0; i < maxPredictions; i++) {
-          const classPrediction =
-              prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-          labelContainer.childNodes[i].innerHTML = classPrediction;
-        maxi = Math.max(maxi, classPrediction);
-      }
-    console.log(maxi);
+    if(prediction[maxIndex].className == aplhabets[aplhabetIndex]){
+        aplhabetIndex = (aplhabetIndex + 1) % aplhabets.length;
+        aplhabetImage.src = "../Assets/Alphabets/" + aplhabets[aplhabetIndex] + ".png";
+        console.log("../Assets/Alphabets/" + aplhabets[aplhabetIndex] + ".png")
+        2
     }
+}
 
-    function draw(results) {
-      // console.log("Called draw function")
-      canvasCtx.save();
-      canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    
-    
-      // Only overwrite missing pixels.
-      canvasCtx.globalCompositeOperation = 'destination-atop';
-      canvasCtx.drawImage(
-          results.image, 0, 0, canvasElement.width, canvasElement.height);
-    
-      canvasCtx.globalCompositeOperation = 'source-over';
-      //drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
-        //              {color: '#00FF00', lineWidth: 4});
-      //drawLandmarks(canvasCtx, results.poseLandmarks,
-      //              {color: '#FF0000', lineWidth: 2});
+function draw(results) {
+    // console.log("Called draw function")
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+
+    // Only overwrite missing pixels.
+    canvasCtx.globalCompositeOperation = 'destination-atop';
+    canvasCtx.drawImage(
+        results.image, 0, 0, canvasElement.width, canvasElement.height);
+
+    canvasCtx.globalCompositeOperation = 'source-over';
+    //drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
+    //              {color: '#00FF00', lineWidth: 4});
+    //drawLandmarks(canvasCtx, results.poseLandmarks,
+    //              {color: '#FF0000', lineWidth: 2});
     //
-      //drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION,
-       //               {color: '#C0C0C070', lineWidth: 1});
-      drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS,
-                      {color: '#CC0000', lineWidth: 5});
-      drawLandmarks(canvasCtx, results.leftHandLandmarks,
-                    {color: '#00FF00', lineWidth: 2});
-      drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS,
-                      {color: '#00CC00', lineWidth: 5});
-      drawLandmarks(canvasCtx, results.rightHandLandmarks,
-                    {color: '#FF0000', lineWidth: 2});
-      canvasCtx.restore();
-    }
-    
+    //drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION,
+    //               {color: '#C0C0C070', lineWidth: 1});
+    drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS,
+        { color: '#CC0000', lineWidth: 5 });
+    drawLandmarks(canvasCtx, results.leftHandLandmarks,
+        { color: '#00FF00', lineWidth: 2 });
+    drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS,
+        { color: '#00CC00', lineWidth: 5 });
+    drawLandmarks(canvasCtx, results.rightHandLandmarks,
+        { color: '#FF0000', lineWidth: 2 });
+    canvasCtx.restore();
+}
+
